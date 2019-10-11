75f4866dd7647e466c6974a574d4eb8dc5707226
victor-pp <victor.pristello@gmail.com>
2019-10-11 11:24:48 -0300

    11/10

--- Byte-Bank/modelo/Cliente.cs
+++ Byte-Bank/modelo/Cliente.cs
@@ -2,24 +2,38 @@ namespace Byte_Bank.modelo
 {
     public class Cliente
     {
-        // ATRIBUTOS 
-        public string Cpf {get; set; }
-        public string Nome {get; set; }
-        public string Email {get; set; }
-        public string Senha {get; set; }
-
-        // CONSTRUTOR
+        private string _cpf;
+        private string _nome;
+        private string _email;
+        private string _senha;
+        public string Cpf{
+            get{return _cpf;}
+            set{_cpf = value;}
+        }
+        public string Nome{
+            get{return _nome;}
+            set{_nome = value;}
+        }
+        public string Email{
+            get{return _email;}
+            set{_email = value;}
+        }
+        public string Senha{
+            get{return _senha;}
+            set{}
+        }
         public Cliente(string Nome, string Cpf, string Email){
             this.Nome = Nome;
             this.Cpf = Cpf;
             this.Email = Email;
         }
-        public bool TrocarSenha(string senha){
-            if((senha.Length > 6)&&(senha.Length < 16)){
-                this.Senha = senha;
-                return true;
-            }else {
-                return false;
+
+        public bool TrocaSenha(string senha){
+            if((senha.Length > 6) && (senha.Length < 16)){
+            this.Senha = senha;
+            return true;
+            } else {
+            return false;
             }
         }
     }
---
+++ Byte-Bank/modelo/ContaCorrente.cs
@@ -0,0 +1,44 @@
+namespace Byte_Bank.modelo
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular {get;set;}
+        public int Agencia {get;set;} 
+        public int Numero {get;set;}
+        private double _saldo;
+        public double Saldo{
+            get { return _saldo; }
+        }
+
+        public ContaCorrente(int Agencia,int Numero,Cliente Titular){
+            this.Agencia = Agencia;
+            this.Numero = Numero;
+            this.Titular = Titular;
+            this._saldo = 0.0;
+        }
+
+        public double Deposito(double valor){
+            if(valor > 0){
+                return false;
+            }else{
+                return true;
+            }
+        }
+        public bool Saque(double valor){
+            if(valor <= this._saldo){
+                this._saldo -= valor;
+                return true;
+            } else{
+                return false;
+            }
+        }
+        public bool Transferencia(ContaCorrente destino, double valor){
+            if (this.Saque(valor)){
+                destino.Deposito(valor);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
